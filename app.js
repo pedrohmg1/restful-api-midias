e5c1a8fbd3f5c126d5e0baf0f3e030b9472fbc63
pedrohmg1 <pedrohmgiacomo@gmail.com>
2025-11-21 16:43:13 -0300

    versão serverless para Vercel

--- app.js
+++ app.js
@@ -1,55 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const logger = require("morgan");
-const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const auth = require("./auth");
-
-const autorRouter = require("./routes/autorRouter");
-const livroRouter = require("./routes/livroRouter");
-const cdRouter = require("./routes/cdRouter");
-const dvdRouter = require("./routes/dvdRouter");
 
 const app = express();
 
+// Middlewares
+app.use(cors());
+app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/biblioteca", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// Rotas
+const autorRoutes = require("./routes/autor");
+const livroRoutes = require("./routes/livro");
+const dvdRoutes = require("./routes/dvd");
+const cdRoutes = require("./routes/cd");
 
+app.use("/autores", autorRoutes);
+app.use("/livros", livroRoutes);
+app.use("/dvds", dvdRoutes);
+app.use("/cds", cdRoutes);
 
+// Conexão MongoDB – Você PRECISA trocar por um MongoDB Atlas
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB conectado"))
+  .catch((err) => console.log("Erro MongoDB:", err));
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-}));
-
-
-app.use(logger("dev"));
-app.use(express.json()); 
-app.use(cookieParser());
-
-
-
-app.use((req, res, next) => {
-  if (req.method === "GET") return next();
-  return auth(req, res, next);
-});
-
-
-app.use("/autores", autorRouter);
-app.use("/livros", livroRouter);
-app.use("/cds", cdRouter);
-app.use("/dvds", dvdRouter);
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`🚀 API rodando em http://localhost:${PORT}`);
-    console.log(`Frontend deve estar em http://localhost:5173`);
-});
-
-
+// Exportar app para Vercel
 module.exports = app;
+
